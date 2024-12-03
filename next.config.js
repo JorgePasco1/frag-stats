6d7d765ac5073a556ff3f7b5fbc291a6d281df5e
Jorge Pasco <jorgepascosoto@gmail.com>
2024-12-03 14:05:57 -0500

    fix hostname

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ await import("./src/env.js");
 /** @type {import("next").NextConfig} */
 const config = {
   images: {
-    domains: ["fimgs.net", "scentlover.shop", "tfs.io"],
+    domains: ["fimgs.net", "scentlover.shop", "utfs.io"],
   },
 };
 
