ba9b7b171d82a488221c1b7d0b5d99fb48a38545
Jorge Pasco <jorgepascosoto@gmail.com>
2025-02-04 12:01:00 -0500

    fix popover on dialog issue

--- src/components/ui/form.tsx
+++ src/components/ui/form.tsx
@@ -155,14 +155,14 @@ const FormMessage = React.forwardRef<
   }
 
   return (
-    <p
+    <div
       ref={ref}
       id={formMessageId}
       className={cn("text-[0.8rem] font-medium text-destructive", className)}
       {...props}
     >
       {body}
-    </p>
+    </div>
   );
 });
 FormMessage.displayName = "FormMessage";
